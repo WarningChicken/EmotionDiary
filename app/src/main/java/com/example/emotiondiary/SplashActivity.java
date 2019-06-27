cb5116b4bfc66594b8c6dd1ddc40406b1596a4da
HyuTlyLy <dptjrgh123@naver.com>
2019-06-27 15:03:13 +0900

    Add Comment

--- app/src/main/java/com/example/emotiondiary/SplashActivity.java
+++ app/src/main/java/com/example/emotiondiary/SplashActivity.java
@@ -9,13 +9,17 @@ public class SplashActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        /*
+        * 파일 시스템 구현 필요
+        * */
         try {
-            Thread.sleep(4000);
+            Thread.sleep(4000); // 4초간 슬립
         }catch (InterruptedException e){
             e.printStackTrace();
         }
-
+        /*
+        파일 시스템에 이미 존해하면 RegisterPage 에는 들어가지 못하도록 구동 필요
+         */
         startActivity(new Intent(this, MainActivity.class));
         finish();
     }
